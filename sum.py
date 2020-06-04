5730c41874286062aa1071f88284b286deafa8c0
Brandon Boatright <bboatright013@gmail.com>
2020-06-04 14:54:04 -0400

    first commit

---
+++ sum.py
@@ -0,0 +1,16 @@
+def sum_nums(nums):
+    """Given list of numbers, return sum of those numbers.
+
+    For example:
+      sum_nums([1, 2, 3, 4])
+
+    Should return (not print):
+      10
+    """  
+    sum = 0
+    for number in nums:
+      sum += number
+    return sum
+
+
+print("sum_nums returned", sum_nums([1, 2, 3, 4, 5]))
